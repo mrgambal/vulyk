f6b57572f4dbd5ecfa5ff8d3833ba0a266874bc0
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-03-22 16:05:37 +0200

    Stitching together plugin and framework with some JS

--- vulyk/static/scripts/base.js
+++ vulyk/static/scripts/base.js
@@ -4,6 +4,7 @@
 var Vulyk = Vulyk || {
         State: {
             workplace: null,
+            body: null,
             task_type: "",
             task_title: "",
             task_id: 0,
@@ -17,19 +18,17 @@ var Vulyk = Vulyk || {
 
             vu.State.workplace
                 .on("click", "a#save-button", function (e) {
-                    e.preventDefault();
+                    vus.body.trigger("vulyk.save", vu.save_report.bind(vu));
                 })
-                .on("click", ".mfp-close", function (e) {
-                    e.preventDefault();
-
-                    vu.State.workplace.find(".mfp-hide").hide();
+                .on("click", "a#skip-button", function (e) {
+                    vus.body.trigger("vulyk.skip", vu.skip_task.bind(vu));
                 });
+
 
             // be able to call methods fluently
             return vu;
         },
-        remap_text: function () {
-        },
+
         load_next: function () {
             var vus = Vulyk.State,
                 meta = null;
@@ -37,17 +36,11 @@ var Vulyk = Vulyk || {
             $.get(
                 "/type/" + vus.task_type + "/next",
                 function (data) {
-                    vus.task_wrapper.html(data);
-
-                    meta = $.parseJSON(
-                        vus.task_wrapper
-                            .find("#task_meta")
-                            .attr('data-payload'));
-                    vus.task_id = meta.id;
-                    vus.task_title = meta.title;
-                    vus.task_init_state = vus.task_state = meta.structure;
-                },
-                "html");
+                    vus.body.trigger("vulyk.next", data);
+                }
+            ).fail(function() {
+                alert("error");
+            });
         },
         show_types_selector: function () {
             // Born to die
@@ -78,12 +71,15 @@ var Vulyk = Vulyk || {
                 "json");
         },
         save_report: function () {
-            var vus = Vulyk.State;
-            // TODO: Breathe the life into this barren method.
+            var
+                vu = this,
+                vus = vu.State;
             $.post(
                 "/type/" + vus.task_type + "/done" + vus.task_id,
                 {},
-                function(data){},
+                function(data) {
+                    vu.load_next()
+                },
                 "json");
         },
         /* http://xkcd.com/292/ */
@@ -94,8 +90,14 @@ var Vulyk = Vulyk || {
 
             $(function () {
                 vu.State.workplace = $(".site-wrapper");
+                vu.State.body = $(document.body);
                 vu.State.task_wrapper = vu.State.workplace.find("#current_task");
                 vu.event_handlers();
+
+                if (vu.State.task_wrapper.length) {
+                    vu.State.task_type = vu.State.task_wrapper.data("type")
+                    vu.load_next();
+                }
 
                 $('.popup-with-zoom-anim').magnificPopup({
                     type: 'inline',
