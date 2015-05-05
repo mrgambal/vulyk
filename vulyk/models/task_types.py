10168fad6e990194542f6fdfe4373899f0beeb2d
Dmitry Gambal <dgambal@conversionscience.co.uk>
2015-05-06 01:13:32 +0300

    FIX: ```--export-all``` now exports all tasks instead of non-closed only

--- vulyk/models/task_types.py
+++ vulyk/models/task_types.py
@@ -100,9 +100,10 @@ class AbstractTaskType(object):
         :returns: Generator of lists of dicts with results
         :rtype: __generator[list[dict]]
         """
+        query = Q(batch=batch, closed=closed) if closed else Q(batch=batch)
 
         if qs is None:
-            qs = self.task_model.objects(batch=batch, closed=closed)
+            qs = self.task_model.objects(query)
 
         for task in qs:
             yield [answer.as_dict()
