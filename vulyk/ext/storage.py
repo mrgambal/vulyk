87bfb131d05eae5592b73d4b3eef29c1322558a2
mrgambal <xbreeder59x@gmail.com>
2015-05-28 03:34:08 +0300

    Serving static files for plugins #23
    do not forget to add this to your local_settings
    
    ```
    COLLECT_STATIC_ROOT = "<full_path_to_dir>"
    COLLECT_STORAGE = 'vulyk.ext.storage'
    ```

---
+++ vulyk/ext/storage.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""Copy files from all static folders to root folder."""
+
+import os
+
+from flask.ext.collect.storage.file import Storage as FileStorage
+from werkzeug.utils import import_string
+
+
+class PluginWrapper(object):
+    """
+    Creates Blueprint-like wrapper for our plugins
+    """
+    def __init__(self, plugin_name, root_url='/static', prefix=''):
+        """
+        Let's stub some dumb fields to get this one treated like a regular
+        Flask blueprint while collecting static files.
+
+        :param plugin_name: Plugin module name
+        :type plugin_name: str
+        :param root_url: Root static URL
+        :type root_url: str
+        """
+        static_path = import_string(plugin_name).__path__[0]
+        static_path = os.path.join(static_path, 'static')
+
+        self.name = '{}{}'.format(prefix, plugin_name)
+        self.static_url_path = '{}/{}/static'.format(root_url, self.name)
+        self.static_folder = ''
+        self.has_static_folder = os.path.isdir(static_path)
+
+        if self.has_static_folder:
+            self.static_folder = static_path
+
+
+class Storage(FileStorage):
+    """Storage that copies static files."""
+
+    def __init__(self, collect, verbose=False):
+        """
+        Copy current array of blueprints and store it.
+        Substitute the dict with the extended one which contains our plugins
+        wrapped into a blueprint-like objects (PluginWrapper).
+
+        Having that done allows standard FileStorage.run() iterate over
+        static files inside of our plugins as well and collect them properly.
+        """
+        super(Storage, self).__init__(collect, verbose)
+
+        blueprints = self.collect.app.blueprints
+        self.old_blueprints = blueprints.copy()
+        self.collect.app.blueprints = self._convert_plugins(blueprints)
+
+    def _convert_plugins(self, blueprints):
+        """
+        Here we create stubs for our plugins with blueprint-like interface.
+
+        :param blueprints: blueprints dict
+        :type blueprints: dict
+
+        :return: Blueprints and our wrappers for plugins
+        :rtype: dict
+        """
+        enabled_tasks = self.collect.app.config.get('ENABLED_TASKS', {})
+        prefix = self.collect.app.config.get('COLLECT_PLUGIN_DIR_PREFIX', '')
+        static_url = self.collect.app.static_url_path
+
+        for name, _ in enabled_tasks.iteritems():
+            plugin = PluginWrapper(name, static_url, prefix)
+            blueprints[name] = plugin
+
+        return blueprints
+
+    def __del__(self):
+        """
+        Restore initial list of blueprints and remove the copy.
+        """
+        self.collect.app.blueprints = self.old_blueprints.copy()
+        self.old_blueprints = None
--- vulyk/tasks/__init__.py
+++ vulyk/tasks/__init__.py
@@ -11,7 +11,7 @@ def init_tasks(app):
     Extracts modules (task types) from global configuration
 
     :param app: Current Flask application instance
-    :type app: Flask.Flask
+    :type app: flask.Flask
 
     :return: Dictionary with instantiated *TaskType objects
     :rtype: dict
@@ -32,7 +32,20 @@ def init_tasks(app):
             "{plugin_name}.models.task_types.{task}".format(
                 plugin_name=plugin, task=task)
         )
-        static_path = import_string(plugin).__path__[0]
+
+        loaders[task_instance.type_name] = jinja2.PackageLoader(plugin)
+        task_types[task_instance.type_name] = task_instance(settings=settings)
+
+        # if Flask-Collect is enabled - get files from collected dir
+        if 'COLLECT_STATIC_ROOT' in app.config:
+            # all plugin static goes stored in a dir may have prefixed name
+            # to prevent any collision e.g. plugin named 'images'
+            prefix = app.config.get('COLLECT_PLUGIN_DIR_PREFIX', '')
+            static_path = os.path.join(app.static_folder,
+                                       '{}{}'.format(prefix, plugin))
+        # else - use standard static folder
+        else:
+            static_path = task_instance.__path__[0]
 
         js_name = 'plugin_js_{task}'.format(task=task_instance.type_name)
         css_name = 'plugin_css_{task}'.format(task=task_instance.type_name)
@@ -40,20 +53,17 @@ def init_tasks(app):
         if len(task_instance.JS_ASSETS) > 0:
             js = Bundle(*map(lambda x: os.path.join(static_path, x),
                              task_instance.JS_ASSETS),
-                        output="scripts/{name}.js".format(name=js_name),
+                        output='scripts/{name}.js'.format(name=js_name),
                         filters=app.config.get('JS_ASSETS_FILTERS', ''))
             app.assets.register(js_name, js)
 
         if len(task_instance.CSS_ASSETS) > 0:
             css = Bundle(*map(lambda x: os.path.join(static_path, x),
                               task_instance.CSS_ASSETS),
-                         output="styles/{name}.css".format(name=css_name),
+                         output='styles/{name}.css'.format(name=css_name),
                          filters=app.config.get('CSS_ASSETS_FILTERS', ''))
 
             app.assets.register(css_name, css)
-
-        loaders[task_instance.type_name] = jinja2.PackageLoader(plugin)
-        task_types[task_instance.type_name] = task_instance(settings=settings)
 
     app.jinja_loader = jinja2.ChoiceLoader([
         app.jinja_loader,
