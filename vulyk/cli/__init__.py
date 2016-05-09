6ccfcc80ccd2a0316606921e08f180df4361428c
Alexander Sapronov <sapronov.alexander92@gmail.com>
2016-05-10 00:44:46 +0600

    Fix manage command - init Added manage command - ru

--- vulyk/cli/__init__.py
+++ vulyk/cli/__init__.py
@@ -6,11 +6,13 @@ def project_init(allowed_types):
     """
     :type allowed_types: list[basestring]
     """
-    if is_initialized():
+    gr_key = 'default'
+    if is_initialized(gr_key) and \
+            list(allowed_types) == Group.objects.get(id=gr_key).allowed_types:
         return
 
     group = Group(
-        id='default',
+        id=gr_key,
         description='default group',
         allowed_types=allowed_types)
 
@@ -19,8 +21,8 @@ def project_init(allowed_types):
     User.objects.update(add_to_set__groups=group)
 
 
-def is_initialized():
+def is_initialized(default_key='default'):
     """
     :rtype: bool
     """
-    return Group.objects(id='default').count() == 1
+    return Group.objects(id=default_key).count() == 1
