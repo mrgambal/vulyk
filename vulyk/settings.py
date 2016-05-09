affcf1aec994a70ebd0f071ed69fc901d225af1e
Alexander Sapronov <sapronov.alexander92@gmail.com>
2016-05-10 00:42:34 +0600

    Big changes.
    Move base templates to base/*
    Added key LANGUAGE and some different translations for string
    Added keys SITE_LOGO and SITE_TITLE
    Added key THANKS_TASK_MESSAGE
    Fixed template for navigate.

--- vulyk/settings.py
+++ vulyk/settings.py
@@ -86,3 +86,9 @@ DEFAULT_BATCH = 'default'
 WARM_WELCOME = u"""
 <h3>Вас вітає Канцелярська сотня.</h3>
 """
+
+SITE_LOGO = ENV('SITE_LOGO', '/static/images/logo.png')
+SITE_TITLE = ENV('SITE_TITLE', u'Канцелярская сотня представляет!')
+
+TEMPLATE_BASE_FOLDERS = []
+THANKS_TASK_MESSAGE = u'Спасибо за помощь, сознательный гражданин!'
